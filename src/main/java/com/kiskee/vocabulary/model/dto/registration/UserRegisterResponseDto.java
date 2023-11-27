ed41bc17914dba9f18cafa7f90976f1066604789
K1SKEE <wenzelx.valve@gmail.com>
2023-11-27 21:58:54 +0200

    minor changes and fix bugs

--- src/main/java/com/kiskee/vocabulary/model/dto/registration/UserRegisterResponseDto.java
+++ src/main/java/com/kiskee/vocabulary/model/dto/registration/UserRegisterResponseDto.java
@@ -2,10 +2,12 @@ package com.kiskee.vocabulary.model.dto.registration;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class UserRegisterResponseDto {
--- src/main/java/com/kiskee/vocabulary/web/controller/registration/RegistrationController.java
+++ src/main/java/com/kiskee/vocabulary/web/controller/registration/RegistrationController.java
@@ -6,7 +6,11 @@ import com.kiskee.vocabulary.service.registration.RegistrationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
