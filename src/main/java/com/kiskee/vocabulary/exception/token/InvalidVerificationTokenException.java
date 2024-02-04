83f11738f983511d2a7d649ab8e590b1efbe430b
K1SKEE <wenzelx.valve@gmail.com>
2024-02-04 14:10:38 +0200

    fix user account activation

---
+++ src/main/java/com/kiskee/vocabulary/exception/token/InvalidVerificationTokenException.java
@@ -0,0 +1,9 @@
+package com.kiskee.vocabulary.exception.token;
+
+public class InvalidVerificationTokenException extends RuntimeException {
+
+    public InvalidVerificationTokenException(String message) {
+        super(message);
+    }
+    
+}
