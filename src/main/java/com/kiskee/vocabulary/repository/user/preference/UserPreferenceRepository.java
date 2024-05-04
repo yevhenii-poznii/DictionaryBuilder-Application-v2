517badd693a37a05805d7ee3feefc562977bd936
Yevhenii <wenzelx.valve@gmail.com>
2024-05-04 18:18:42 +0300

    fix

--- src/main/java/com/kiskee/vocabulary/repository/user/preference/UserPreferenceRepository.java
+++ src/main/java/com/kiskee/vocabulary/repository/user/preference/UserPreferenceRepository.java
@@ -1,6 +1,5 @@
 package com.kiskee.vocabulary.repository.user.preference;
 
-import com.kiskee.vocabulary.model.dto.user.preference.DictionaryPreferenceDto;
 import com.kiskee.vocabulary.model.entity.user.preference.UserPreference;
 import com.kiskee.vocabulary.repository.user.BaseUserProfilePreferenceRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,7 +8,4 @@ import java.util.UUID;
 
 public interface UserPreferenceRepository
         extends BaseUserProfilePreferenceRepository, JpaRepository<UserPreference, UUID> {
-
-    DictionaryPreferenceDto findDictionaryPreferenceByUserId(UUID userId);
-
 }
