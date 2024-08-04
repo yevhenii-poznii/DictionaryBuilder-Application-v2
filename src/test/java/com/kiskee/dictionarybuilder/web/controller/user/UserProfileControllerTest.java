af7c0407d200b3f15a940bd2618535dfc264470d
Yevhenii <wenzelx.valve@gmail.com>
2024-08-04 20:08:46 +0300

    add tests

---
+++ src/test/java/com/kiskee/dictionarybuilder/web/controller/user/UserProfileControllerTest.java
@@ -0,0 +1,61 @@
+package com.kiskee.dictionarybuilder.web.controller.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kiskee.dictionarybuilder.model.dto.user.profile.UserMiniProfileDto;
+import com.kiskee.dictionarybuilder.service.user.profile.UserProfileService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserProfileController.class)
+public class UserProfileControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserProfileService userProfileService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc =
+                MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    @SneakyThrows
+    void testGetMiniProfile_WhenProfileExists_ThenReturnUserMiniProfileDto() {
+        UserMiniProfileDto miniProfile = new UserMiniProfileDto("username", "someEncodedPicture");
+
+        when(userProfileService.getMiniProfile()).thenReturn(miniProfile);
+
+        MvcResult result = mockMvc.perform(get("/user/profile/me/mini"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String actualResponseBody = result.getResponse().getContentAsString();
+        assertThat(actualResponseBody).isEqualTo(objectMapper.writeValueAsString(miniProfile));
+    }
+}
