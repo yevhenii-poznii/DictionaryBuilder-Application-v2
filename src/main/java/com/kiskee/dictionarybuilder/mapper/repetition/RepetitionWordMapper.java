e743f2d4aaaf1db6f798aff3b70b280dc64ccde8
K1SKEE <wenzelx.valve@gmail.com>
2024-09-10 14:19:12 +0300

    fix mapper

--- src/main/java/com/kiskee/dictionarybuilder/mapper/repetition/RepetitionWordMapper.java
+++ src/main/java/com/kiskee/dictionarybuilder/mapper/repetition/RepetitionWordMapper.java
@@ -6,16 +6,13 @@ import com.kiskee.dictionarybuilder.model.entity.redis.repetition.RepetitionData
 import com.kiskee.dictionarybuilder.model.entity.vocabulary.Word;
 import java.util.List;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface RepetitionWordMapper {
 
     List<WordDto> toDto(List<Word> repetitionWords);
 
-    @Mapping(source = "repetitionData.wordHint", target = "wordHint")
     WSResponse toWSResponse(RepetitionDataDto repetitionData);
 
-    @Mapping(source = "repetitionData.wordHint", target = "wordHint")
     WSResponse toWSResponse(RepetitionDataDto repetitionData, Long correctTranslationsCount);
 }
